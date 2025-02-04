8effcf729122e0640e5be9a5233fbb6df9673262
Pierre-antoine Comby <pierre-antoine.comby@crans.org>
2025-02-04 16:18:06 +0100

    feat: add CI job.

--- extra/update_notebook_header.py
+++ extra/update_notebook_header.py
@@ -5,15 +5,11 @@ dependencies to run the notebook.
 """
 
 import os
-import json
 from pathlib import Path
-from copy import deepcopy
 import argparse
 import glob
-from typing import Any
 
 import nbformat as nbf
-from nbformat import NotebookNode
 
 
 def _arg2path(p:str) -> Path:
@@ -22,18 +18,21 @@ def _arg2path(p:str) -> Path:
 if __name__ == "__main__":
     # Get all the notebooks in the src directory
     parser = argparse.ArgumentParser()
-    parser.add_argument("header_notebook", type=_arg2path, default=Path(__file__).parent() / "header_colab.ipynb")
-    parser.add_argument("notebook_dir", type=_arg2path, default=Path(__file__).parent() / 'src')
-
+    parser.add_argument("--header_notebook", type=_arg2path, default=Path(__file__).parent / "header_colab.ipynb")
+    parser.add_argument("--notebook_dir", type=_arg2path, default=Path(__file__).parent.parent / 'src')
+    parser.add_argument("--inplace", action=argparse.BooleanOptionalAction, default=False)
     args = parser.parse_args()
     os.chdir(args.notebook_dir)
 
 
     header_nbk = nbf.read(args.header_notebook, as_version=4)
     for notebook_file in glob.glob("*.ipynb"):
-        nbk = nbf.read(notebook_file)
+        nbk = nbf.read(notebook_file, as_version=4)
         nbk.cells = [nbk.cells[0], *header_nbk.cells, *nbk.cells[1:]]
 
-        nbf.write(nbk, notebook_file)
+        if args.inplace:
+            nbf.write(nbk, notebook_file)
+        else:
+            nbf.write(nbk, Path(notebook_file).stem + "_updated.ipynb")
 
     # in the CI pipeline, these new notebook will be uploaded to a new branch
